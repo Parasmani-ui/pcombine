98f093c7d1e87de9a2a62ffa87c5f20d13eeb5a2
Vikash Lanjhikar <vikashlanjhi@bhu.ac.in>
2025-10-14 03:58:21 +0530

    server-index2

--- server/index.js
+++ server/index.js
@@ -129,7 +129,7 @@ app.use((req, res, next) => {
   }
 
   const mongoUri = process.env.MONGODB_URI || `mongodb://127.0.0.1:27017`;
-  const dbName = databases[host];
+  const dbName = databases[callerKey];
   const client = new MongoClient(mongoUri);
   client.connect()
     .then(() => {
